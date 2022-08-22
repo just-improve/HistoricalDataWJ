7545453ed3f6bf79fb99f22827711c66361d22c2
just-improve <janik.w777@gmail.com>
2022-08-22 10:31:30 +0200

    Metoda pobierająca dane historyczne z volumenem

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def get_historical_candles(symbol, resolution, iterations):
     file = open(file_name, 'w', newline='')
     dict_writer = csv.DictWriter(file, keys)
     dict_writer.writeheader()
-
+    print("before for ")
     for x in range(iterations):
         response = ftx_client.get_historical_future_candles(symbol, resolution, start_time, end_time)
         response.reverse()
@@ -73,10 +73,50 @@ def get_historical_candles(symbol, resolution, iterations):
         start_time = end_time - one_day_in_seconds
 
     file.close()
+    print("finished")
+
+def get_his_candles_wj (symbol, resolution, iterations):
+    ftx_client = FtxClient()
+
+    end_time = time.time()
+    start_time = end_time - one_day_in_seconds
+
+    keys = ['open', 'high', 'low', 'close','volume', 'startDate', 'startTime']
+    file_name = "ftx-" + symbol + "-" + list(RESOLUTION.keys())[list(RESOLUTION.values()).index(resolution)] + ".csv"
+    file = open(file_name, 'w', newline='')
+    dict_writer = csv.DictWriter(file, keys)
+    dict_writer.writeheader()
+    print("before for ")
+    for x in range(iterations):
+        response = ftx_client.get_historical_prices(symbol, resolution, start_time, end_time)
+        #response.reverse()
+
+        new_list = []
+
+        for r in response:
+            d = dict()
+            d['open'] = r['open']
+            d['high'] = r['high']
+            d['low'] = r['low']
+            d['close'] = r['close']
+            d['volume'] = r['volume']
+            dt = parser.parse(r['startTime'])
+            d['startDate'] = dt.strftime("%Y/%m/%d")
+            d['startTime'] = dt.strftime("%H:%M:%S")
+            new_list.append(d)
+            print(d)
+        dict_writer.writerows(new_list)
+        #print(new_list)
+        end_time -= one_day_in_seconds
+        start_time = end_time - one_day_in_seconds
+
+    file.close()
+    print("finished")
 
 
 if __name__ == '__main__':
-    # get_historical_candles()
+    get_his_candles_wj("AMPL-PERP",60,1)
+
 
     #symbolArg = "BTC" # str(sys.argv[1])   #odczytuje btc jako arg 1
     #resolutionArg = RESOLUTION["5m"] # RESOLUTION[str(sys.argv[2])]
@@ -87,7 +127,7 @@ if __name__ == '__main__':
     #print("FINISHED")
 
     #ftx_client = FtxClient()
-    get_funding_rates(3)
+    #get_funding_rates(3)
     #listMy = ftx_client.get_all_futures()
     #orderBook = ftx_client.get_orderbook("1INCH-PERP",2)
     #print(orderBook)
