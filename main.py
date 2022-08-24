1554db3564ea2cce324525378c4c7097fc969c9a
just-improve <janik.w777@gmail.com>
2022-08-24 11:44:32 +0200

    Metoda pobierająca dane 15 s również

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import itertools
 import sys
 import time
 
@@ -11,6 +12,7 @@ fifteen_days_in_seconds = 1296000
 ten_days_in_seconds = 864000
 one_day_in_seconds = 86400
 six_hours_in_seconds = 21600
+one_hour_in_seconds = 3600
 
 RESOLUTION = {"15s": 15, "1m": 60, "5m": 300, "1h": 3600, "1d": 86400}
 
@@ -80,6 +82,46 @@ def get_his_candles_wj (symbol, resolution, iterations):
 
     file.close()
     print("finished")
+#one_hour_in_seconds
+
+def get_15s_candles_wj (symbol, resolution, iterations):
+    ftx_client = FtxClient()
+    days = iterations
+    iterations = iterations*4
+    end_time = time.time()
+    start_time = end_time - six_hours_in_seconds
+
+    keys = ['open', 'high', 'low', 'close','volume', 'startDate', 'startTime']
+    file_name = symbol + "-" + list(RESOLUTION.keys())[list(RESOLUTION.values()).index(resolution)]+" " + str(days) +" dni v15s"+ ".csv"
+    file = open(file_name, 'w', newline='')
+    dict_writer = csv.DictWriter(file, keys)
+    dict_writer.writeheader()
+    print("before for ")
+    for x in range(iterations):
+        response = ftx_client.get_historical_prices(symbol, resolution, start_time, end_time)
+        response.reverse()
+
+        new_list = []
+        for r in response:
+
+            d = dict()
+            d['open'] = r['open']
+            d['high'] = r['high']
+            d['low'] = r['low']
+            d['close'] = r['close']
+            d['volume'] = r['volume']
+            dt = parser.parse(r['startTime'])
+            d['startDate'] = dt.strftime("%Y/%m/%d")
+            d['startTime'] = dt.strftime("%H:%M:%S")
+            new_list.append(d)
+            #print(d)
+        dict_writer.writerows(new_list)
+        #print(new_list)
+        end_time -= six_hours_in_seconds
+        start_time = end_time - six_hours_in_seconds
+
+    file.close()
+    print("finished")
 
 
 def get_15s_candles_funding_wj (symbol, resolution, iterations):
@@ -87,7 +129,7 @@ def get_15s_candles_funding_wj (symbol, resolution, iterations):
     days = iterations
     iterations = iterations*4
     end_time = time.time()
-    start_time = end_time - six_hours_in_seconds
+    start_time = end_time - one_day_in_seconds
 
     keys = ['open', 'high', 'low', 'close','volume', 'startDate', 'startTime','rate']
     file_name = symbol + "-" + list(RESOLUTION.keys())[list(RESOLUTION.values()).index(resolution)]+" " + str(days) +" dni v15s funding"+ ".csv"
@@ -100,9 +142,14 @@ def get_15s_candles_funding_wj (symbol, resolution, iterations):
         response.reverse()
         response_funding= ftx_client.get_all_funding_rates(start_time, end_time, symbol)
         response_funding.reverse()
+        print(response_funding)
 
         new_list = []
+        #for (r, f) in itertools.zip_longest(response, response_funding):
+        #for (r, f) in map(None, response, response_funding):
         for (r, f) in zip(response, response_funding):
+        #for (r, f) in izip(response, response_funding):
+        #for r in response:
 
             d = dict()
             d['open'] = r['open']
@@ -118,15 +165,15 @@ def get_15s_candles_funding_wj (symbol, resolution, iterations):
             #print(d)
         dict_writer.writerows(new_list)
         #print(new_list)
-        end_time -= six_hours_in_seconds
-        start_time = end_time - six_hours_in_seconds
+        end_time -= one_day_in_seconds
+        start_time = end_time - one_day_in_seconds
 
     file.close()
     print("finished")
 
 
 if __name__ == '__main__':
-    get_15s_candles_funding_wj("AMPL-PERP", 300, 1)
+    get_15s_candles_funding_wj("ETH-PERP", 300, 1)
 
 
     #symbolArg = "BTC" # str(sys.argv[1])   #odczytuje btc jako arg 1
