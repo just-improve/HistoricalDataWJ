7ce456ca2d22e62a2e2be648760d2ca4ec8896fe
just-improve <janik.w777@gmail.com>
2022-08-24 11:08:13 +0200

    Metoda pobierająca dane 15 s również

--- main.py
+++ main.py
@@ -39,48 +39,6 @@ def get_funding_rates(iterations):
     file.close()
 
 
-def get_historical_candles(symbol, resolution, iterations):
-    ftx_client = FtxClient()
-
-    end_time = time.time()
-    start_time = end_time - one_day_in_seconds
-    number_of_days = iterations
-
-    # keys = ['close', 'high', 'low', 'open', 'startDate', 'startTime', 'volume', 'time']
-    keys = ['close', 'high', 'low', 'open', 'startDate', 'startTime']
-    file_name = symbol + "-" + list(RESOLUTION.keys())[list(RESOLUTION.values()).index(resolution)] + str(number_of_days) +" dni "+ ".csv"
-    file = open(file_name, 'w', newline='')
-    dict_writer = csv.DictWriter(file, keys)
-    dict_writer.writeheader()
-    print("before for ")
-    for x in range(iterations):
-        response = ftx_client.get_historical_future_candles(symbol, resolution, start_time, end_time)
-        response.reverse()
-
-        new_list = []
-
-        for r in response:
-            d = dict()
-            d['close'] = r['close']
-            d['high'] = r['high']
-            d['low'] = r['low']
-            d['open'] = r['open']
-            dt = parser.parse(r['startTime'])
-            d['startDate'] = dt.strftime("%Y/%m/%d")
-            d['startTime'] = dt.strftime("%H:%M:%S")
-            new_list.append(d)
-            print(d)
-        dict_writer.writerows(new_list)
-        #print(new_list)
-        end_time -= one_day_in_seconds
-        start_time = end_time - one_day_in_seconds
-
-    file.close()
-    print("finished")
-
-
-
-
 def get_his_candles_wj (symbol, resolution, iterations):
     ftx_client = FtxClient()
 
@@ -130,24 +88,22 @@ def get_15s_candles_wj (symbol, resolution, iterations):
     iterations = iterations*4
     end_time = time.time()
     start_time = end_time - six_hours_in_seconds
-    number_of_days = iterations
 
-    keys = ['open', 'high', 'low', 'close','volume', 'startDate', 'startTime']
-    file_name = symbol + "-" + list(RESOLUTION.keys())[list(RESOLUTION.values()).index(resolution)]+" " + str(days) +" dni v15s"+ ".csv"
+    keys = ['open', 'high', 'low', 'close','volume', 'startDate', 'startTime','rate']
+    file_name = symbol + "-" + list(RESOLUTION.keys())[list(RESOLUTION.values()).index(resolution)]+" " + str(days) +" dni v15s funding"+ ".csv"
     file = open(file_name, 'w', newline='')
     dict_writer = csv.DictWriter(file, keys)
     dict_writer.writeheader()
     print("before for ")
-    #pobiera 1500 rekordów a na minutówce mamy 1440 rekordów więc pobierze 1440 rekordów i przejdzie do następnego dnia i znowu pobierzez 1440 rekordów
-    #w przypadku 15 sekundóek pobierze 1500 a to nie jest całość więc powinien pobrać co
-    #15s  = 5760 rekordów
     for x in range(iterations):
         response = ftx_client.get_historical_prices(symbol, resolution, start_time, end_time)
         response.reverse()
+        response_funding= ftx_client.get_all_funding_rates(start_time, end_time, symbol)
+        response_funding.reverse()
 
         new_list = []
+        for (r, f) in zip(response, response_funding):
 
-        for r in response:
             d = dict()
             d['open'] = r['open']
             d['high'] = r['high']
@@ -157,6 +113,7 @@ def get_15s_candles_wj (symbol, resolution, iterations):
             dt = parser.parse(r['startTime'])
             d['startDate'] = dt.strftime("%Y/%m/%d")
             d['startTime'] = dt.strftime("%H:%M:%S")
+            d['rate'] = f['rate']
             new_list.append(d)
             #print(d)
         dict_writer.writerows(new_list)
@@ -169,7 +126,7 @@ def get_15s_candles_wj (symbol, resolution, iterations):
 
 
 if __name__ == '__main__':
-    get_15s_candles_wj("ETH-PERP",15,70)
+    get_15s_candles_wj("AMPL-PERP",300,1)
 
 
     #symbolArg = "BTC" # str(sys.argv[1])   #odczytuje btc jako arg 1
@@ -180,10 +137,3 @@ if __name__ == '__main__':
     #get_historical_candles(symbolArg, resolutionArg, iterationsArg)
     #print("FINISHED")
 
-    #ftx_client = FtxClient()
-    #get_funding_rates(3)
-    #listMy = ftx_client.get_all_futures()
-    #orderBook = ftx_client.get_orderbook("1INCH-PERP",2)
-    #print(orderBook)
-    #print(orderBook["bids"])
-    #print("hello guys")
