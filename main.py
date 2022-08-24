d728937c77fe44e41324b3b4a0398642c4ce7b23
just-improve <janik.w777@gmail.com>
2022-08-24 06:44:32 +0200

    Metoda pobierająca dane historyczne z volumenem

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 import sys
 import time
 
@@ -20,6 +21,7 @@ def get_funding_rates(iterations):
     end_time = time.time()
     start_time = end_time - one_day_in_seconds
 
+
     keys = ['future', 'rate', 'time']
 
     file = open('ftx-funding-rates.csv', 'w', newline='')
@@ -37,84 +39,50 @@ def get_funding_rates(iterations):
     file.close()
 
 
-def get_historical_candles(symbol, resolution, iterations):
+def get_his_candles_wj (symbol, resolution,ilosc_dni):
     ftx_client = FtxClient()
+    ilosc_dni_w_sekundach = 86400*ilosc_dni
+    ten_days_in_seconds_wj = 864000
+    one_day_in_seconds_wj = 86400
 
+    time_now = datetime.datetime.now()
+    str_time_now = str(time_now)
     end_time = time.time()
-    start_time = end_time - one_day_in_seconds
-
-    # keys = ['close', 'high', 'low', 'open', 'startDate', 'startTime', 'volume', 'time']
-    keys = ['close', 'high', 'low', 'open', 'startDate', 'startTime']
-    file_name = symbol + "-" + list(RESOLUTION.keys())[list(RESOLUTION.values()).index(resolution)] + ".csv"
-    file = open(file_name, 'w', newline='')
-    dict_writer = csv.DictWriter(file, keys)
-    dict_writer.writeheader()
-    print("before for ")
-    for x in range(iterations):
-        response = ftx_client.get_historical_future_candles(symbol, resolution, start_time, end_time)
-        response.reverse()
-
-        new_list = []
-
-        for r in response:
-            d = dict()
-            d['close'] = r['close']
-            d['high'] = r['high']
-            d['low'] = r['low']
-            d['open'] = r['open']
-            dt = parser.parse(r['startTime'])
-            d['startDate'] = dt.strftime("%Y/%m/%d")
-            d['startTime'] = dt.strftime("%H:%M:%S")
-            new_list.append(d)
-            print(d)
-        dict_writer.writerows(new_list)
-        #print(new_list)
-        end_time -= one_day_in_seconds
-        start_time = end_time - one_day_in_seconds
-
-    file.close()
-    print("finished")
-
-def get_his_candles_wj (symbol, resolution, iterations):
-    ftx_client = FtxClient()
-
-    end_time = time.time()
-    start_time = end_time - one_day_in_seconds
+    start_time = end_time - ilosc_dni_w_sekundach
 
     keys = ['open', 'high', 'low', 'close','volume', 'startDate', 'startTime']
-    file_name = symbol + "-" + list(RESOLUTION.keys())[list(RESOLUTION.values()).index(resolution)] + ".csv"
+    file_name = symbol + "-" + list(RESOLUTION.keys())[list(RESOLUTION.values()).index(resolution)] +" "+ str(ilosc_dni)+" dni "+  ".csv"
     file = open(file_name, 'w', newline='')
     dict_writer = csv.DictWriter(file, keys)
     dict_writer.writeheader()
     print("before for ")
-    for x in range(iterations):
-        response = ftx_client.get_historical_prices(symbol, resolution, start_time, end_time)
-        response.reverse()
 
-        new_list = []
+    response = ftx_client.get_historical_prices(symbol, resolution, start_time, end_time)
+    response.reverse()
 
-        for r in response:
-            d = dict()
-            d['open'] = r['open']
-            d['high'] = r['high']
-            d['low'] = r['low']
-            d['close'] = r['close']
-            d['volume'] = r['volume']
-            dt = parser.parse(r['startTime'])
-            d['startDate'] = dt.strftime("%Y/%m/%d")
-            d['startTime'] = dt.strftime("%H:%M:%S")
-            new_list.append(d)
-            #print(d)
-        dict_writer.writerows(new_list)
+    new_list = []
+
+    for r in response:
+        d = dict()
+        d['open'] = r['open']
+        d['high'] = r['high']
+        d['low'] = r['low']
+        d['close'] = r['close']
+        d['volume'] = r['volume']
+        dt = parser.parse(r['startTime'])
+        d['startDate'] = dt.strftime("%Y/%m/%d")
+        d['startTime'] = dt.strftime("%H:%M:%S")
+        new_list.append(d)
+    dict_writer.writerows(new_list)
         #print(new_list)
-        end_time -= one_day_in_seconds
-        start_time = end_time - one_day_in_seconds
+
 
     file.close()
     print("finished")
 
 
 if __name__ == '__main__':
+    #pass
     get_his_candles_wj("BTC-PERP",60,5)
 
 
