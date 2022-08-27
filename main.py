d439570bc3f3b5c2c10982ae1b968c90960c2867
just-improve <janik.w777@gmail.com>
2022-08-27 11:13:05 +0200

    Próba zrobienia łączenia danych

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ ten_days_in_seconds = 864000
 one_day_in_seconds = 86400
 six_hours_in_seconds = 21600
 
-RESOLUTION = {"15s": 15, "1m": 60, "5m": 300, "1h": 3600, "1d": 86400}
+RESOLUTION = {"15s": 15, "1m": 60, "5m": 300, "15m": 900, "1h": 3600, "1d": 86400}
 
 
 #oryginalna metody piotrka
@@ -217,10 +217,107 @@ def get_1h_candles_with_funding_wj (symbol, iterations):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#do danych 15s
+def return_list_from_file (file_name):
+    #file_name = "ETH-PERP-1h 1 dni v15s.csv"
+    file = open(file_name, 'r')
+    list_as_file = []
+    for x in file:
+        list_as_file.append(x)
+    #print(list_as_file)
+    file.close()
+    return list_as_file
+
+#zwraca dicta z key and values
+def create_new_data_list(symbol,resolution, iterations):
+    ftx_client = FtxClient()
+    #iterations = iterations * 4
+    end_time = time.time()
+    start_time = end_time - six_hours_in_seconds
+    ftx_client = FtxClient()
+    new_list = []
+    for x in range(iterations):
+        response = ftx_client.get_historical_prices(symbol, resolution, start_time, end_time)
+        response.reverse()
+        local_list = []
+        print(response)
+        print("nowy response")
+        for r in response:
+            local_list.append(r['open'])
+            local_list.append(r['high'])
+            local_list.append(r['low'])
+            local_list.append(r['close'])
+            local_list.append(r['high'])
+            local_list.append(r['volume'])
+            dt = parser.parse(r['startTime'])
+            local_list.append(dt.strftime("%Y/%m/%d"))
+            local_list.append(dt.strftime("%H:%M:%S"))
+
+            new_list.append(local_list)
+        end_time -= six_hours_in_seconds
+        start_time = end_time - six_hours_in_seconds
+
+    return new_list
+
+
+
+def get_15s_candles_wj_overriden (symbol, resolution, iterations, file_name):
+    list_of_new_data = create_new_data_list(symbol, resolution, iterations)
+    list_from_existing_file = return_list_from_file("ETH-PERP-1h 1 dni v15s.csv")
+
+
+
+def remove_common(a, b):
+    for i in a[:]:
+        if i in b:
+            a.remove(i)
+            b.remove(i)
+
+    #for x in a:
+        #print (x)
+    #for x in b:
+        #print(x)
+
+
 if __name__ == '__main__':
-    get_1h_candles_with_funding_wj("ETH-PERP", 2)
+    #get_15s_candles_wj_overriden("ETH-PERP", 3600, 1)
+    #return_list_from_file("ETH-PERP-1h 1 dni v15s.csv")
+    list_from_existing_file = return_list_from_file("ETH-PERP-1h 1 dni v15s.csv")
+    list_of_new_data = create_new_data_list("ETH-PERP", 3600, 1)
 
 
+    #print(list_from_existing_file[1])
+
+    #for x in list_of_new_data:
+        #print(x)
+
+    #print(list_of_new_data)
+
+
+    #print(len(list_from_existing_file))
+    #print(len(list_of_new_data))
+
+    #remove_common (list_from_existing_file, list_of_new_data )
+
+
+
+
+
+    li = [1,2,3,4,5,6]
+    li2 = [4,5,6,7,8,9,10]
     #symbolArg = "BTC" # str(sys.argv[1])   #odczytuje btc jako arg 1
     #resolutionArg = RESOLUTION["5m"] # RESOLUTION[str(sys.argv[2])]
     #iterationsArg = 2 # int(sys.argv[3])
